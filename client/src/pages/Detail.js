e94a2de7e97ea8e6917640ab60b0c7482afacd1b
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-09-10 16:27:25 -0400

    added redux hooks and updated the Detail function to inlcude hooks

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,24 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-// TODO add the Redux hooks useDispatch and useSelector
+// Added the Redux hooks useDispatch and useSelector
+import { useDispatch, useSelector } from "react-redux";
 
 import Cart from "../components/Cart";
 import { useStoreContext } from "../utils/GlobalState";
-import {
-  REMOVE_FROM_CART,
-  UPDATE_CART_QUANTITY,
-  ADD_TO_CART,
-  UPDATE_PRODUCTS,
-} from "../utils/actions";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
 import { idbPromise } from "../utils/helpers";
 import spinner from "../assets/spinner.gif";
 
 function Detail() {
-  // TODO useDispatch handles dispatch and useSelector handles state
-  // ? Are there any other places that we've used context? These will also need to be replaced in the same way.
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
@@ -95,20 +90,13 @@ function Detail() {
           <p>{currentProduct.description}</p>
 
           <p>
-            <strong>Price:</strong>${currentProduct.price}{" "}
-            <button onClick={addToCart}>Add to Cart</button>
-            <button
-              disabled={!cart.find((p) => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
+            <strong>Price:</strong>${currentProduct.price} <button onClick={addToCart}>Add to Cart</button>
+            <button disabled={!cart.find((p) => p._id === currentProduct._id)} onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
+          <img src={`/images/${currentProduct.image}`} alt={currentProduct.name} />
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
